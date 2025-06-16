006f684dbcef0a7cde7006b54a328069ec7f509b
Billy <billy.petersson@innofactor.com>
2025-06-16 20:36:42 +0200

    Update project metadata and configuration files; remove commented code in Program.cs

--- Myfunction/Program.cs
+++ Myfunction/Program.cs
@@ -9,8 +9,6 @@ var host = new HostBuilder()
     .ConfigureFunctionsWebApplication()
     .ConfigureServices(services =>
     {
-        // För Managed Identity
-        // var storageAccountName = Environment.GetEnvironmentVariable("stfuncmyfunctionapp1");
         var serviceUri = new Uri($"https://stfuncmyfunctionapp1.table.core.windows.net");
 
         services.AddSingleton(serviceProvider =>
